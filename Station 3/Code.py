4f3c6049b087c0eeb10f014f2e45dd35fa30f071
DaZorro75 <44848344+DaZorro75@users.noreply.github.com>
2023-12-26 11:33:28 +0100

    Stationscode für Station 3 hinzugefügt.

---
+++ Station 3/Code.py
@@ -0,0 +1,12 @@
+AnzahlSchlangen = 0
+
+def on_button_pressed_a():
+    global AnzahlSchlangen
+    AnzahlSchlangen = 4
+    for index in range(AnzahlSchlangen):
+        for index2 in range(5):
+            basic.show_icon(IconNames.SNAKE)
+            basic.pause(1000)
+            basic.clear_screen()
+            basic.pause(1000)
+input.on_button_pressed(Button.A, on_button_pressed_a)
