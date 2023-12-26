c7b3c16d6b02d2e571208eb277c300ba4202b381
DaZorro75 <44848344+DaZorro75@users.noreply.github.com>
2023-12-26 11:57:48 +0100

    Codefix für Station 4.

--- Station 4/Code.py
+++ Station 4/Code.py
@@ -2,7 +2,7 @@ Stein1 = 2
 Stein2 = 4
 Stein3 = 5
 Stein4 = 8
-if Stein1 * (Stein1 / (Stein1 + Stein1)) == 12:
+if Stein1 * Stein1 / Stein1 + Stein1 == 12:
     basic.show_icon(IconNames.HAPPY)
 else:
     basic.show_icon(IconNames.SAD)
