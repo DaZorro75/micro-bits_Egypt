a1b9dfb7a4851e29e2ad1f4a2fe81b5b5b147004
DaZorro75 <44848344+DaZorro75@users.noreply.github.com>
2023-12-26 11:04:10 +0100

    Added Students' Code for Station 1

---
+++ Station1/Code.py
@@ -0,0 +1,24 @@
+def on_button_pressed_a():
+    led.plot(SchritteNachRechts, SchritteNachUnten)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    led.plot(0, 0)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+SchritteNachRechts = 0
+SchritteNachUnten = 0
+SchritteNachUnten = 0
+SchritteNachRechts = 0
+SchritteNachRechts += 0
+SchritteNachRechts += 0
+SchritteNachRechts += 0
+SchritteNachUnten += 0
+SchritteNachUnten += 0
+SchritteNachRechts += 0
+SchritteNachUnten += 0
+SchritteNachRechts += 0
+SchritteNachRechts += 0
+SchritteNachUnten += 0
+SchritteNachRechts += 0
+SchritteNachUnten += 0
